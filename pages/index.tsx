6130f677c4d36b4ff030d9e2bce2443f5f8c25d3
Axolotlpi <cfc3.1415@gmail.com>
2022-02-23 16:17:47 -0600

    compose main page with graphql querying

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import type { NextPage } from 'next';
 import BaseLayout from '../layouts/BaseLayout';
+import BaseHead from '../layouts/BaseHead';
+import { getContent } from '../utils/helpers';
+import { LandingPage, queries, SiteSettings } from '../utils/queries';
 
-const Home: NextPage = () => {
+interface Props {
+  siteSettings: SiteSettings;
+  landingPage: LandingPage;
+}
+
+export async function getStaticProps() {
+  //fetches return double wrapped objs, getting inner one eg. siteSettings.siteSettings
+  const [{ siteSettings }, { landingPage }] = await Promise.all([
+    getContent(queries.siteSettings),
+    getContent(queries.landingPage),
+  ]);
+  return {
+    props: { siteSettings, landingPage },
+  };
+}
+
+const Home: NextPage<Props> = ({ siteSettings, landingPage }) => {
   return (
     <BaseLayout>
-      <main></main>
+      <BaseHead {...siteSettings} />
+      <main>
+        <h1>{landingPage.mainTitle}</h1>
+      </main>
 
       <footer></footer>
     </BaseLayout>
