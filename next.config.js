482ac486570e5fffb1ce61ad87a45e912ed9b687
Axolotlpi <cfc3.1415@gmail.com>
2022-02-23 16:14:30 -0600

    setup dotenv

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const Dotenv = require('dotenv-webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
