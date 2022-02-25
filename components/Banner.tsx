9ed88f48e45708e7e5e10f8b9ed0d35c1fa9d577
Axolotlpi <cfc3.1415@gmail.com>
2022-02-25 17:59:30 -0600

    style banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -13,15 +13,18 @@ interface Props {
 const Banner: NextPage<Props> = ({ title, teaser, image }) => {
   return (
     <>
-      <h1>{title}</h1>
-      <p>{teaser}</p>
-      <div className="relative w-4/5 h-4/5">
+      <div className="relative h-[40vh] sm:h-[50vh] md:h-[50vh] lg:h-[50vh] xl:h-[60vh] w-full">
         <Image
+          className="-z-40"
           src={image.src}
           alt={image.description}
           layout="fill"
           objectFit="cover"
         />
+      </div>
+      <div className="bg-accent-1 flex flex-col md:flex-row justify-center items-end">
+        <h1 className="p-4 text-primary-0">{title}</h1>
+        <p className="p-4 font-bold bg-accent-2 text-primary-3">{teaser}</p>
       </div>
     </>
   );
