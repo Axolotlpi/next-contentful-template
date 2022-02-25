1eb155436c947b08e103ff950a977d6dd6aaf152
Axolotlpi <cfc3.1415@gmail.com>
2022-02-25 17:59:19 -0600

    add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        'primary-0': '#ffffff',
+        'primary-1': '#D9D9D9',
+        'primary-2': '#A1A5A6',
+        'primary-3': '#353D40',
+        'accent-1': '#003F63',
+        'accent-2': '#F2B138',
+      },
       fontFamily: {
         sans: ['Source Code Pro', 'Tahoma', 'sans-serif'],
         heading: ['Montserrat Alternates', 'Arial', 'sans-serif'],
